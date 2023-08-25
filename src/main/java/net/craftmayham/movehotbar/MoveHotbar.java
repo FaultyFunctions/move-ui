c2419e5a84368960e370827eafe33e95b8b92d08
craftmayham <craftmayham@gmail.com>
2023-08-25 16:48:09 -0400

    Added stuff

---
+++ src/main/java/net/craftmayham/movehotbar/MoveHotbar.java
@@ -0,0 +1,138 @@
+package net.craftmayham.movehotbar;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.api.ClientModInitializer;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+
+public class MoveHotbar implements ClientModInitializer {
+    public static final String MOD_ID = "move-hotbar";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "movehotbar.json");
+	public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	public static Config config = new Config();
+
+	public static class Config {
+
+		public int hotbarx = 0;
+		public int hotbary = 0;
+		public int textx = 0;
+		public int texty = 0;
+		public int expx = 0;
+		public int expy = 0;
+		public int expdx=0;
+		public int expdy=0;
+		public int healthx=0;
+		public int healthy=0;
+		public int healthspacing=0;
+		public int hungerx=0;
+		public int hungery=0;
+		public int hungerspacing=0;
+	}
+
+	public static void saveConfig() {
+		try {
+			FileWriter fileWriter = new FileWriter(file);
+			fileWriter.write(gson.toJson(getConfig()));
+			fileWriter.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void loadConfig() {
+		try {
+			if (file.exists()) {
+				config = gson.fromJson(Files.readString(file.toPath()), Config.class);
+			} else {
+				config = new Config();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		setConfig(config);
+	}
+
+	public static void setConfig(Config config) {
+		MoveHotbar.config = config;
+	}
+	public static Config getConfig() {
+		return config;
+	}
+	public static int getHotbarX() {
+		return config.hotbarx;
+	}
+	public static int getHotbarY() {
+		return config.hotbary;
+	}
+	public static int getTextX() {
+		return config.textx;
+	}
+	public static int getTextY() {
+		return config.texty;
+	}
+	public static int getExpX() {
+		return config.expx;
+	}
+	public static int getExpY() {
+		return config.expy;
+	}
+	public static int getExpDX() {
+		return config.expdx;
+	}
+	public static int getExpDY() {
+		return config.expdy;
+	}
+	public static int getHealthX() {
+		return config.healthx;
+	}
+	public static int getHealthY() {
+		return config.healthy;
+	}
+	public static int getHealthSpacing() {
+		return config.healthspacing;
+	}
+	public static int getHungerX() {
+		return config.hungerx;
+	}
+	public static int getHungerY() {
+		return config.hungery;
+	}
+	public static int getHungerSpacing() {
+		return config.hungerspacing;
+	}
+
+	public static void putObjects() {
+		FabricLoader.getInstance().getObjectShare().put("movehotbar:x", config.hotbarx);
+		FabricLoader.getInstance().getObjectShare().put("movehotbar:y", config.hotbary);
+		FabricLoader.getInstance().getObjectShare().put("text:x", config.textx);
+		FabricLoader.getInstance().getObjectShare().put("text:y", config.texty);
+		FabricLoader.getInstance().getObjectShare().put("exp:x", config.expx);
+		FabricLoader.getInstance().getObjectShare().put("exp:y", config.expy);
+		FabricLoader.getInstance().getObjectShare().put("expd:x", config.expdx);
+		FabricLoader.getInstance().getObjectShare().put("expd:y", config.expdy);
+		FabricLoader.getInstance().getObjectShare().put("health:x", config.healthx);
+		FabricLoader.getInstance().getObjectShare().put("health:y", config.healthy);
+		FabricLoader.getInstance().getObjectShare().put("health:spacing", config.healthspacing);
+		FabricLoader.getInstance().getObjectShare().put("hunger:x", config.hungerx);
+		FabricLoader.getInstance().getObjectShare().put("hunger:y", config.hungery);
+		FabricLoader.getInstance().getObjectShare().put("hunger:spacing", config.hungerspacing);
+	}
+	@Override
+	public void onInitializeClient() {
+		if (!file.exists()) {
+			saveConfig();
+		}
+		loadConfig();
+		putObjects();
+	}
+
+}
---
+++ src/main/java/net/craftmayham/movehotbar/mixin/client/gui/hud/InGameHudMixin.java
@@ -0,0 +1,91 @@
+package net.craftmayham.movehotbar.mixin.client.gui.hud;
+
+import net.craftmayham.movehotbar.MoveHotbar;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+@Mixin(value = InGameHud.class, priority = -1)
+public class InGameHudMixin {
+    @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 1)
+    private int modifyHotbarX(int value) {
+        return value + MoveHotbar.getHotbarX();
+    }
+    @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 2)
+    private int modifyHotbarY(int value) {
+        return value - MoveHotbar.getHotbarY();
+    }
+    @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(Lnet/minecraft/client/gui/DrawContext;IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V"), index = 1)
+    private int modifyItemX(int value) {
+        return value + MoveHotbar.getHotbarX();
+    }
+    @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(Lnet/minecraft/client/gui/DrawContext;IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V"), index = 2)
+    private int modifyItemY(int value) {
+        return value - MoveHotbar.getHotbarY();
+    }
+    @ModifyVariable(method = "renderHeldItemTooltip", at = @At(value = "STORE"), ordinal = 1)
+    private int modifyHeldItemTooltipX(int value) {
+        return value + MoveHotbar.getTextX();
+    }
+    @ModifyVariable(method = "renderHeldItemTooltip", at = @At(value = "STORE"), ordinal = 2)
+    private int modifyHeldItemTooltipY(int value) {
+        return value - MoveHotbar.getTextY();
+    }
+    @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 1)
+    private int modifyExperienceBarX(int value) {
+        return value + MoveHotbar.getExpX();
+    }
+    @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 2)
+    private int modifyExperienceBarY(int value) {
+        return value - MoveHotbar.getExpY();
+    }
+    @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;IIIZ)I"), index = 2)
+    private int modifyXpDX(int value) {
+        return value + MoveHotbar.getExpDX();
+    }
+    @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;IIIZ)I"), index = 3)
+    private int modifyXpDY(int value) {
+        return value - MoveHotbar.getExpDY();
+    }
+    @ModifyArg(method = "renderMountJumpBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 1)
+    private int modifyJumpX(int value) {
+        return value + MoveHotbar.getExpX();
+    }
+    @ModifyArg(method = "renderMountJumpBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 2)
+    private int modifyJumpY(int value) {
+        return value - MoveHotbar.getExpY();
+    }
+    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHealthBar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;IIIIFIIIZ)V"), index = 2)
+    private int modifyHealthBarX(int value) {
+        return value + MoveHotbar.getHealthX();
+    }
+    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHealthBar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;IIIIFIIIZ)V"), index = 3)
+    private int modifyHealthBarY(int value) {
+        return value - MoveHotbar.getHealthY();
+    }
+    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHealthBar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/entity/player/PlayerEntity;IIIIFIIIZ)V"), index = 4)
+    private int modifyHealthSpacing(int value) {
+        return value + MoveHotbar.getHealthSpacing();
+    }
+    @ModifyArg(method = "renderMountHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 1)
+    private int modifyMountHealthX(int value) {
+        return value + MoveHotbar.getHungerX();
+    }
+    @ModifyArg(method = "renderMountHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 2)
+    private int modifyMountHealthY(int value) {
+        return value - MoveHotbar.getHungerY();
+    }
+    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 1)
+    private int modifyStatusBarsX(int value) {
+        return value + MoveHotbar.getHungerX();
+    }
+    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 2)
+    private int modifyStatusBarsY(int value) {
+        return value - MoveHotbar.getHungerY();
+    }
+    @ModifyArg(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"), index = 2)
+    private int modifyStatusBarsSpacing(int value) {
+        return value + MoveHotbar.getHungerSpacing();
+    }
+}
